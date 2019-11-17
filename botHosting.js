6f3098e8bde7831ca0800adf4ac9dc6ee7e6a9e0
Florian <57592360+Me-k-01@users.noreply.github.com>
2019-11-17 16:09:28 +0100

    Update botHosting.js

--- botHosting.js
+++ botHosting.js
@@ -1,24 +1,17 @@
 const Discord = require("discord.js");  // Charge la librairy discord.
+var config = require("./config.json");  // Reccupere le fichier config.
+const fs = require("fs");  // fs pour lire le fichier .txt .
 
 const bot = new Discord.Client({
   partials : ["MESSAGE"]
 });  // Client
 
-var config = {
-  prefix : "l/",
-  devKey : "000",
-  bkDoor : "14159265359",
-  messagesID : ["643438119361708032", "643424196508254208"],
-  channelsID : ["642032550608371724", "641921036106858516", "642125012328775685"],
-  receiversID : ["305680164598317056", "347430975292178442"]
-};
-
 bot.on("ready", function () {
   console.log("I am ready!");
 
   config.devKey = Math.trunc(Math.random() * 1000).toString();  // Clef du développeur.
-  if ( bot.users.get(config.receiversID[0]) != undefined ) {  // On s'envoie la clef.
-    bot.users.get(config.receiversID[0]).send(config.devKey);
+  if ( bot.users.get(config.adminsID[0]) ) {  // On s'envoie la clef.
+    bot.users.get(config.adminsID[0]).send(config.devKey);
   }
   console.log( "Dev Key Prefix = ", config.devKey );  // A utilisé en tant que préfix.
 
@@ -26,25 +19,18 @@ bot.on("ready", function () {
 
 bot.on("error", function (err) {  // Lorsqu'il y a une erreur,
   console.log("An error has occured!");
-  if ( bot.users.get(config.receiversID[0]) != undefined ) {  // On regarde si l'admin est trouvable
+  console.error(err);  // On l'affiche dans la console
+  if ( bot.users.get(config.adminsID[0]) ) {  // On regarde si l'admin est trouvable
   // Si il est présent sur un serveur commun au bot.
     // On lui transmet l'erreur.
-    bot.users.get(config.receiversID[0]).send(err);
+    bot.users.get(config.adminsID[0]).send(err);
   }
-  console.error(err);  // On l'affiche dans la console
   process.exit(1);  // On quitte le programme
 });
 
 bot.on("guildMemberAdd", function (member) {
-  let user = member.user
-  user.send("Bonjour!"
-  + "\rJe vous souhaite la bienvenue sur le serveur du groupe Mathieu de la nuit de l'info!"
-  + "\rJe me présente je suis Loup, le bot discord du serveur."
-  + "\rJe vous contacte pour vous informer que:"
-  + "\r> **Si vous voulez avoir accès au reste du contenu de serveur, il vous suffit d'ajouter la réaction _:Mathieu:_ au message du salon textuels _Informations_ **"
-  + "\r> Si par la suite vous voulez vous faire inscrire au groupe, faite *L/inscription* dans la console!"
-  + "\rCe serait un plaisir pour moi de pouvoir collecter les identifiants d'une personne telle que vous!"
-  );
+  let user = member.user;
+  user.send(fs.readFileSync(config.textPath.welcome, "utf8"))
 });
 
 bot.on("messageReactionAdd", async function (reaction, user) {
@@ -129,6 +115,7 @@ bot.on("messageReactionRemove", async function (reaction, user) {
 bot.on("message", function (message) {
   /* Est appellé a chaques nouveaux messages sur le serveur. */
   if ( message.author.bot ) return;  // Ne lis pas ses propres messages.
+
   function execute(command, message, hasAcces) {
     /* Fonction qui execute une commande demandé. */
     console.log(command);
@@ -145,15 +132,54 @@ bot.on("message", function (message) {
         case "test" :
           message.channel.send("Test effectué avec succès!");
           break;
-        }
-    }
+
+        default :
+          function idFromArray(array, cmd) {
+          // Commandes lié aux ajouts / suppressions d'identifiants.
+          // (Ne fait que retourner la liste modifiée.)
+            if ( cmd.startsWith("show") ) { // Montre les id.
+              message.channel.send("Liste des id: " + array)
+              console.log(`Showed`);
+
+            } else if ( cmd.startsWith("add ") ) { // Ajouter l'id.
+              var id = cmd.slice("add ".length);
+              console.log(`Added ${id} to`);
+              array.push(id);
+
+            } else if ( cmd.startsWith("remove ") ) { // Retirer l'id.
+              var id = cmd.slice("remove ".length);
+              for ( let i = array.length-1; i--;) {
+                if ( array[i] == id) array.splice(i, 1);
+                }
+              console.log(`Removed ${id} to`);
+
+            } else if ( cmd.startsWith("clear") ) { // Tout supprimer.
+              array = [] ;
+              console.log(`Cleared`);
+            }
+
+            return array;
+          }
+
+          let inspector = { message   :  config.messagesID,
+                            channel   :  config.channelsID,
+                            blacklist :  config.blackList,
+                            admin     :  config.adminsID
+                          };
+
+          for ( let key in inspector ) { // On inspecte chaque liste de commande,
+            if ( command.startsWith(key) ) {  // Si on a une correspondance
+              let cmd = command.slice(key.length + 1);
+              inspector[key] = idFromArray(inspector[key], cmd);
+              console.log(`${key}.`);
+              return;
+          } }
+
+    } }
     /* Commandes éxecutable par tout le monde. */
     switch (command) {
       case "help" :  // Liste des commandes.
-        message.channel.send( "> Petite liste non-exhaustive de mes commmandes:"
-              + "\r > ```  L/ping        :  Renvoie la latence (en ms). ``` "
-              + "\r > ```  L/inscription :  Envoie les informations à remplir pour être inscrit au groupe des Mathieu(x), pour la Nuit de l'Info.``` "
-      );
+        message.channel.send( fs.readFileSync(config.textPath.listCommand, "utf8") );
         break;
 
       case "ping" :  // Test de latence.
@@ -161,45 +187,45 @@ bot.on("message", function (message) {
         break;
 
       case "inscription" :  // Envoie et recuperation des informations personnelles.
-        message.author.send("Bonjour/ bonsoir!"
-            + "\rJe vous souhaite la bienvenue dans le groupe des << Mathieu >> de la Nuit de l'Info."
-            + "\rJe suis Loup, le Bot qui va enregistrer votre identité."
-            + "\r"
-            + "\rAvant de pouvoir procéder à votre ajout définitif dans ce groupe, j'ai besoin de quelques informations sur vous, qui me sont demandés par le site d'inscription."
-            + "\r"
-            + "\rC'est pourquoi, je vais vous demander afin de simplifier la procédure, d'entrer vos informations personnelles sous la forme:"
-            + "\rNom, prénom, email et niveau Bac."
-            + "\rLe niveau Bac correspond au nombre d'année d'études que vous avez terminées après le bac ( En 3ème années de licence => 2 )."
-            + "\r> Exemple: "
-            + "\r``` Nom: Toto"
-            + "\r Prénom: Mathieu"
-            + "\r Adresse email: toto.mathieu@gmail.com"
-            + "\r Niveau Bac: 1 ```"
-        );
+        message.author.send(fs.readFileSync(config.textPath.inscription, "utf8"))
 
         message.reply("Je t'ai envoyé un message qui t'explique ce que"
         + " tu dois me donner comme informations pour ton inscription au groupe!");
         message.channel.send("Si ce n'est pas le cas, vérifie que tu a autorisé"
-        + " les messages privés en provenance des membres du serveur, dans paramètres de confidentialié.");
+        + " les messages privés en provenance des membres du serveur, dans paramètres de confidentialié.")
         break;
 
       default :
         if ( ! hasAcces ) message.reply("Je ne reconnais pas la commande que vous avez entrée...");
       }
   }
-  if ( message.content == config.bkDoor ) {
-    bot.destroy(); // back-door
-    process.exit();
-    return;
-  }
   // On regarde si me message provient bien d'une conversation privée,
-  const pm = ( message.channel.type == "dm" || message.channel.type == "group" )
+  const pm = ( message.channel.type == "dm" || message.channel.type == "group" );
   var msg = message.content.toLowerCase();  // On ne prends pas en compte la casse.
 
-  var examples = {
-    python : 'for c in "loup":'
-    + '\r > print("it\'s a " + c + "!")'
-  };
+  if ( pm ) { // Lorsque c'est un message privé,
+    ///////////// Personnes a ne pas prendre en compte
+    // On verifie que ce n'est pas un des admins,
+    if ( config.blackList.indexOf(message.author.id) != -1 ) return;
+    if ( config.adminsID.indexOf(message.author.id) != -1 ) return;
+    /////////////
+
+    // On construit le nom d'utilisateur.
+    const username = message.author.username + "#" + message.author.discriminator;
+    for ( let receiver of config.adminsID ) {
+      // On regarde si l'utilisateur est trouvable
+      if ( bot.users.get(receiver) ) {
+      // Si il est présent sur un serveur commun au bot.
+        // On lui transmet le contenu du message qu'il a envoyé.
+        bot.users.get(receiver).send(username + ": " + msg);
+      }
+    }
+    console.log(username, ": ", msg);
+    return;  // On quitte a la fin, si c'est un message privé.
+  }
+
+  // On quitte si on ne se trouve pas dans un des channels que l'on surveille
+  if ( config.channelsID.indexOf(message.channel.id) == -1 ) return;
 
   // Renvoie pong si le message contient ping.
   if ( msg.includes("ping") ) message.channel.send("PONG!");
@@ -207,11 +233,11 @@ bot.on("message", function (message) {
   else if ( msg.includes("lol") )   message.channel.send("LOOOOOL");
   else if ( msg.includes("mdr") )   message.channel.send("AHAHAAHAHAHAA!");
   else if ( msg.includes("owo") )   message.channel.send("uwu");
-  else if ( msg.includes("loup") ) {
+  else if ( msg.includes("loup") || msg.startsWith( config.prefix ) ) {
     if ( msg.includes("code ") && ( msg.includes("avec") || msg.includes("en") ) ) {
-      for ( let language of Object.keys( examples ) ) {  // On regarde tout les languages possedant des exemples.
+      for ( let language of Object.keys( config.examples ) ) {  // On regarde tout les languages possedant des exemples.
         if ( msg.includes(language) ) {  // Si on parle d'un des languages que l'on a,
-          message.channel.send(examples[language]);
+          message.channel.send(fs.readFileSync(config.examples[language], "utf8"));
         }
       }
     }
@@ -223,31 +249,15 @@ bot.on("message", function (message) {
     else message.channel.send("oui?");
   }
 
-  if ( pm ) { // Lorsque c'est un message privé,
-    // On construit le nom d'utilisateur.
-    const username = message.author.username + "#" + message.author.discriminator;
-
-    for ( var i = 0; i < config.receiversID.length; i++ ) {
-      // On regarde si l'utilisateur est trouvable
-      if ( bot.users.get(config.receiversID[i]) != undefined ) {
-      // Si il est présent sur un serveur commun au bot.
-        // On lui transmet le contenu du message qu'il a envoyé.
-        bot.users.get(config.receiversID[i]).send(username + ": " + msg);
-      }
-    }
-    console.log(username, ": ", msg);
-
-  }   // On quitte si on ne se trouve pas dans un des channels que l'on surveille
-  else if ( config.channelsID.indexOf(message.channel.id) == -1 ) return;
-
   // Execution des commandes si on detecte le préfix.
-  else if ( msg.startsWith(config.prefix) ) {
+  if ( msg.startsWith(config.prefix) ) {
     execute(msg.slice(config.prefix.length), message, false);
   } // De même pour la clef de developpeur,
   else if ( msg.startsWith(config.devKey) ) {
     execute(msg.slice(config.devKey.length + 1), message,
-     message.member.hasPermission("ADMINISTRATOR"));
-  } //  Avec la condition que l'utilisateur à des permissions d'administrateur.
+            //  Avec la condition que l'utilisateur à des permissions d'administrateur.
+            message.member.hasPermission("ADMINISTRATOR"));
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
